abc86700e81fceba2f6fcdf805029f1d737bc573
Linish2020 <linish.kalbande@armorcode.io>
2024-03-28 18:49:44 +0530

    Update BasicAuthenticationExample.java

--- BasicAuthenticationExample.java
+++ BasicAuthenticationExample.java
@@ -1,5 +1,3 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Base64;
@@ -16,29 +14,17 @@ public class BasicAuthenticationExample {
             // Set the request method to GET
             connection.setRequestMethod("GET");
 
-            // Set up Basic Authentication using hardcoded username and password
-            String username = "Johny";
-            String password = "papa";
-
+            // Set up Basic Authentication
+            String username = "jommy";
+            String password = "luck";
             String authString = username + ":" + password;
             String encodedAuthString = Base64.getEncoder().encodeToString(authString.getBytes());
+            // Set the Authorization header
             connection.setRequestProperty("Authorization", "Basic " + encodedAuthString);
 
             // Get the response code
             int responseCode = connection.getResponseCode();
             System.out.println("Response Code: " + responseCode);
-
-            // Read the response
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuilder response = new StringBuilder();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            // Print the response
-            System.out.println("Response: " + response.toString());
 
             // Close the connection
             connection.disconnect();
